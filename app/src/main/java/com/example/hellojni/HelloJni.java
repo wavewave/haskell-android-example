832571d888a2eb087fef324cc2475d8218c942a7
Ian-Woo Kim <ianwookim@gmail.com>
2015-09-27 06:47:35 +0000

    first camera use success!

--- app/src/main/java/com/example/hellojni/HelloJni.java
+++ app/src/main/java/com/example/hellojni/HelloJni.java
@@ -16,19 +16,29 @@
 package com.example.hellojni;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toolbar;
 import android.widget.VideoView;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.util.Log;
+import java.io.IOException;
+import java.util.List;
+
 
 public class HelloJni extends Activity
 {
@@ -38,6 +48,11 @@ public class HelloJni extends Activity
     ListView lv;
     FloatingActionButton fab;
     VideoView vv; 
+
+
+    Camera mCamera;
+    CameraPreview mPreview;
+    private View mCameraView;    
 
     
     Toolbar toolbar;
@@ -57,17 +72,43 @@ public class HelloJni extends Activity
 
 	tv = (TextView) findViewById(R.id.textview1);
 	onCreateHS(tv);
-	addListenerOnButton(); 
+	addListenerOnButton();
 
-        vv = (VideoView) findViewById(R.id.myvideo);
-	String vaddr = "http://ianwookim.org/video/test.mp4";
-	Uri vuri= Uri.parse(vaddr);
-	vv.setVideoURI(vuri);
-	vv.start();
+
+        //vv = (VideoView) findViewById(R.id.myvideo);
+	//String vaddr = "http://ianwookim.org/video/test.mp4";
+	//Uri vuri= Uri.parse(vaddr);
+	//vv.setVideoURI(vuri);
+	//vv.start();
+
+        mCameraView = (View) findViewById(R.id.camera_preview);
+  
 	
+	boolean opened = safeCameraOpenInView( mCameraView );
+
+	if(opened == false ) {
+	    Log.d("CameraGuide", "Error, Camera failed to open");
+	}
+
+
+
+    }
+
+    public boolean safeCameraOpenInView(View view) {
+	boolean qOpened = false;
+	releaseCameraAndPreview();
+	mCamera = getCameraInstance();
+	mCameraView = view;
+	qOpened = (mCamera != null);
+
+        if( qOpened == true ) {
+	    mPreview = new CameraPreview(this.getBaseContext(), mCamera, view);
+	    FrameLayout preview = (FrameLayout)view;
+	    preview.addView(mPreview);
+	    mPreview.startCameraPreview();
+	}
 	
-	//stringFromJNI();
-        //setContentView(tv); */
+        return qOpened;      
     }
 
     
@@ -115,4 +156,91 @@ public class HelloJni extends Activity
     static {
         System.loadLibrary("haskell");
     }
+
+    public static Camera getCameraInstance() {
+	Camera c = null;
+	try {
+	    c = Camera.open();
+	}
+	catch (Exception e) {
+	    e.printStackTrace();
+	}
+	return c;
+    }
+
+    private void releaseCameraAndPreview() {
+	if (mCamera != null) {
+	    mCamera.stopPreview();
+	    mCamera.release();
+	    mCamera = null;
+	}
+	if (mPreview != null) {
+	    mPreview.destroyDrawingCache();
+	    mPreview.mCamera = null;
+	}
+    }
+    
+    
+    class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+	private SurfaceHolder mHolder;
+	private Camera mCamera;
+	private Context mContext;
+	private Camera.Size mPreviewSize;
+	private View mCameraView;
+	private List<Camera.Size> mSupportedPreviewSizes;
+	private List<String> mSupportedFlashModes;
+
+	public CameraPreview( Context context, Camera camera, View cameraView ) {
+	    super(context);
+	    mCameraView = cameraView;
+	    mContext = context;
+	    setCamera(camera);
+	    mHolder = getHolder();
+	    mHolder.addCallback(this);
+	    mHolder.setKeepScreenOn(true);
+	}
+
+        public void startCameraPreview()
+	{
+	    try {
+		mCamera.setPreviewDisplay(mHolder);
+		mCamera.startPreview();
+	    }
+	    catch(Exception e) {
+	     	e.printStackTrace();
+	    }
+	}
+	
+        private void setCamera( Camera camera )
+	{
+	    mCamera = camera;
+	    mSupportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
+	    mSupportedFlashModes = mCamera.getParameters().getSupportedFlashModes();
+
+	    if(mSupportedFlashModes != null && mSupportedFlashModes.contains(Camera.Parameters.FLASH_MODE_AUTO)) {
+		Camera.Parameters parameters = mCamera.getParameters();
+		parameters.setFlashMode(Camera.Parameters.FLASH_MODE_AUTO);
+		mCamera.setParameters(parameters);
+	    }
+	    requestLayout();
+	}
+
+
+        public void surfaceCreated( SurfaceHolder holder ) {
+	    try {
+		mCamera.setPreviewDisplay(holder);
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
+	}
+	
+        public void surfaceDestroyed(SurfaceHolder holder) {
+	    if(mCamera != null) {
+	    }
+	}
+
+	public void surfaceChanged( SurfaceHolder holder, int format, int w, int h ) {
+	}
+	 
+    } 
 }
