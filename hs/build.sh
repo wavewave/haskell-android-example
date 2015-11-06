46345ba97114564465d850cb0b62d249de572d75
Ian-Woo Kim <ianwookim@gmail.com>
2015-11-06 02:06:14 +0000

    make sure that shared object is copied to make this buildable from clean setup.

--- hs/build.sh
+++ hs/build.sh
@@ -1,3 +1,5 @@
+cp $ANDROID_NDK_HOME/sources/cxx-stl/llvm-libc++/libs/armeabi/libc++_shared.so ../app/src/main/jniLibs/armeabi/libc++_shared.so
+
 cd android-bridge ; ./build.sh ; cd ..
 
 cabal --with-ghc=arm-unknown-linux-androideabi-ghc --with-ld=arm-linux-androideabi-ld.gold --with-ghc-pkg=arm-unknown-linux-androideabi-ghc-pkg --extra-include-dir=$ANDROID_NDK_HOME/platforms/android-21/arch-arm/usr/include  --extra-include-dir=$ANDROID_NDK_HOME/sources/cxx-stl/llvm-libc++/libcxx/include --extra-lib-dir=$ANDROID_NDK_HOME/sources/cxx-stl/llvm-libc++/libs/armeabi configure -v3 ;
